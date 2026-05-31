c09ea5755973addcae014d0ed503b6333638a41b
Jonaass <jonascn.2021@gmail.com>
2026-05-31 18:36:43 -0300

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://flash-cards-edw2.onrender.com',
         changeOrigin: true,
       },
     },
